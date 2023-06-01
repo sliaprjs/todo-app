5cc31354a41d2f371dc3b3c7ecc9e06d6a5ae85e
sliaprjs <sliaprjs@gmail.com>
2023-06-01 15:13:24 +0300

    backend deploy

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const { response } = require('express');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -86,7 +88,7 @@ app.post('/api/notes', (req, res) => {
 
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
